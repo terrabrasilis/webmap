2263005a3a8da9bb67bfb49005855d69f79ec844
Paulo Luan <pauloluan.inova@gmail.com>
2019-10-31 10:29:55 -0300

    fixed date picker

--- src/app/tool/layer-filter-tool/layer-filter-tool.component.ts
+++ src/app/tool/layer-filter-tool/layer-filter-tool.component.ts
@@ -1,39 +1,57 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
-import { Layer } from '../../entity/layer';
-import { RegisterComponent } from '../../util/component-decorator';
-import { ToolComponent } from '../tool-component-interface';
-import { OnMount } from '../../core-modules/dynamic-html';
-import { MatDialog } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
-import { LayerFilterComponent } from './layer-filter.component';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef
+} from "@angular/core";
+import { Layer } from "../../entity/layer";
+import { RegisterComponent } from "../../util/component-decorator";
+import { ToolComponent } from "../tool-component-interface";
+import { OnMount } from "../../core-modules/dynamic-html";
+import { MatDialog } from "@angular/material";
+import { DomSanitizer } from "@angular/platform-browser";
+import { LayerFilterComponent } from "./layer-filter.component";
 
 /**
  * LayerFilterToolComponent
  * <layer-filter-tool  [shared]="layer"></layer-filter-tool>
  */
 @Component({
-  selector: 'layer-filter-tool',
+  selector: "layer-filter-tool",
   template: `
-              <dfn attr.data-info="{{ 'tools.layerFilter' | translate }}" #innerContent>
-                <button type="button" class="btn" *ngIf="layer !== null" (click)="showDialog(layer)"><i class="material-icons md-dark ">filter_list</i></button>
-                <ng-content></ng-content>
-              </dfn>
-            `
+    <dfn attr.data-info="{{ 'tools.layerFilter' | translate }}" #innerContent>
+      <button
+        type="button"
+        class="btn"
+        *ngIf="layer !== null"
+        (click)="showDialog(layer)"
+      >
+        <i class="material-icons md-dark ">filter_list</i>
+      </button>
+      <ng-content></ng-content>
+    </dfn>
+  `
 })
-
 @RegisterComponent
-export class LayerFilterToolComponent extends ToolComponent implements OnInit, OnMount {
+export class LayerFilterToolComponent extends ToolComponent
+  implements OnInit, OnMount {
   layer: Layer;
 
   @Input() shared: any;
-  @ViewChild('innerContent', { static: true }) innerContent: ElementRef;
+  @ViewChild("innerContent", { static: true }) innerContent: ElementRef;
 
-  dynamicOnMount(attr: Map <string, any> , innerHTML: string, el: any) {
+  dynamicOnMount(attr: Map<string, any>, innerHTML: string, el: any) {
     this.innerContent.nativeElement.innerHTML = innerHTML;
     this.layer = this.shared;
   }
 
-  constructor(private dialog: MatDialog, private dom: DomSanitizer, private cdRef: ChangeDetectorRef) {
+  constructor(
+    private dialog: MatDialog,
+    private dom: DomSanitizer,
+    private cdRef: ChangeDetectorRef
+  ) {
     super();
   }
 
@@ -47,6 +65,9 @@ export class LayerFilterToolComponent extends ToolComponent implements OnInit, O
 
   showDialog() {
     this.cdRef.detectChanges();
-    this.dialog.open(LayerFilterComponent, { width : '450px', data: {layer: this.layer} });
+    this.dialog.open(LayerFilterComponent, {
+      width: "450px",
+      data: { layer: this.layer }
+    });
   }
 }
--- src/app/tool/layer-filter-tool/layer-filter.component.ts
+++ src/app/tool/layer-filter-tool/layer-filter.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit, Optional, Inject, ChangeDetectorRef } from '@angular/core';
-import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { DomSanitizer } from '@angular/platform-browser';
-import { DialogComponent } from '../../dialog/dialog.component';
-import { TerrabrasilisApiComponent } from '../terrabrasilis-api/terrabrasilis-api.component';
-import { Layer } from '../../entity/layer';
+import {
+  Component,
+  OnInit,
+  Optional,
+  Inject,
+  ChangeDetectorRef
+} from "@angular/core";
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from "@angular/material";
+import { FormBuilder, FormControl, Validators } from "@angular/forms";
+import { DomSanitizer } from "@angular/platform-browser";
+import { DialogComponent } from "../../dialog/dialog.component";
+import { TerrabrasilisApiComponent } from "../terrabrasilis-api/terrabrasilis-api.component";
+import { Layer } from "../../entity/layer";
+import { head, last } from "lodash";
 
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatDatepicker } from '@angular/material/datepicker';
+import { MomentDateAdapter } from "@angular/material-moment-adapter";
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from "@angular/material/core";
+import { MatDatepicker } from "@angular/material/datepicker";
 
 // Depending on whether rollup is used, moment needs to be imported differently.
 // Since Moment.js doesn't have a default export, we normally need to import using the `* as`
 // syntax. However, rollup creates a synthetic default module and we thus need to import it using
 // the `default as` syntax.
-import * as _moment from 'moment';
+import * as _moment from "moment";
 // tslint:disable-next-line:no-duplicate-imports
-import { default as _rollupMoment, Moment } from 'moment';
+import { default as _rollupMoment, Moment } from "moment";
 
 const moment = _rollupMoment || _moment;
 
@@ -24,59 +35,89 @@ const moment = _rollupMoment || _moment;
 // https://momentjs.com/docs/#/displaying/format/
 export const MY_FORMATS = {
   parse: {
-    dateInput: 'MM/YYYY',
+    dateInput: "MM/YYYY"
   },
   display: {
-    dateInput: 'MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
+    dateInput: "MM/YYYY",
+    monthYearLabel: "MMM YYYY",
+    dateA11yLabel: "LL",
+    monthYearA11yLabel: "MMMM YYYY"
+  }
 };
 
-
 @Component({
-  selector: 'layer-filter',
-  templateUrl: './layer-filter.component.html',
-  styleUrls: ['./layer-filter.component.css'],
+  selector: "layer-filter",
+  templateUrl: "./layer-filter.component.html",
+  styleUrls: ["./layer-filter.component.css"],
   providers: [
     // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
     // application's root module. We provide it at the component level here, due to limitations of
     // our example generation script.
-    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE]
+    },
 
-    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
-  ],
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }
+  ]
 })
-
 export class LayerFilterComponent implements OnInit {
-  date = new FormControl(moment());
+  initialDate = new FormControl(moment());
+  endDate = new FormControl(moment());
+  minDate = new Date(1900, 0, 1);
+  maxDate = new Date(2050, 0, 1);
+
   layer: Layer;
 
   constructor(
-    private dialogRef: MatDialogRef < LayerFilterComponent > ,
+    private dialogRef: MatDialogRef<LayerFilterComponent>,
     private dom: DomSanitizer,
     private dialog: MatDialog,
     private cdRef: ChangeDetectorRef,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.layer = data.layer
+    this.layer = data.layer;
   }
 
-  private terrabrasilisApi: TerrabrasilisApiComponent = new TerrabrasilisApiComponent(this.dialog, this.dom, this.cdRef, null);
+  private terrabrasilisApi: TerrabrasilisApiComponent = new TerrabrasilisApiComponent(
+    this.dialog,
+    this.dom,
+    this.cdRef,
+    null
+  );
 
   ngOnInit() {}
 
-  chosenYearHandler(normalizedYear: Moment) {
-    const ctrlValue = this.date.value;
+  chosenYearHandlerInitialDate(normalizedYear: Moment) {
+    const ctrlValue = this.initialDate.value;
     ctrlValue.year(normalizedYear.year());
-    this.date.setValue(ctrlValue);
+    this.initialDate.setValue(ctrlValue);
   }
 
-  chosenMonthHandler(normalizedMonth: Moment, datepicker: MatDatepicker < Moment > ) {
-    const ctrlValue = this.date.value;
+  chosenMonthHandlerInitialDate(
+    normalizedMonth: Moment,
+    datepicker: MatDatepicker<Moment>
+  ) {
+    const ctrlValue = this.initialDate.value;
     ctrlValue.month(normalizedMonth.month());
-    this.date.setValue(ctrlValue);
+    this.initialDate.setValue(ctrlValue);
+    datepicker.close();
+  }
+
+  chosenYearHandlerEndDate(normalizedYear: Moment) {
+    const ctrlValue = this.endDate.value;
+    ctrlValue.year(normalizedYear.year());
+    this.endDate.setValue(ctrlValue);
+  }
+
+  chosenMonthHandlerEndDate(
+    normalizedMonth: Moment,
+    datepicker: MatDatepicker<Moment>
+  ) {
+    const ctrlValue = this.endDate.value;
+    ctrlValue.month(normalizedMonth.month());
+    this.initialDate.setValue(ctrlValue);
     datepicker.close();
   }
 
@@ -89,15 +130,28 @@ export class LayerFilterComponent implements OnInit {
   }
 
   showDialog(content: string): void {
-    const dialogRef = this.dialog.open(DialogComponent, { width: '450px' });
-    dialogRef.componentInstance.content = this.dom.bypassSecurityTrustHtml(content);
+    const dialogRef = this.dialog.open(DialogComponent, { width: "450px" });
+    dialogRef.componentInstance.content = this.dom.bypassSecurityTrustHtml(
+      content
+    );
   }
 
   getDimensions() {
-    this.terrabrasilisApi.getDimensions(this.layer)
-      .then((result) => {
-        console.log(result)
+    const self = this;
+    this.terrabrasilisApi
+      .getDimensions(this.layer)
+      .then(results => {
+        self.handleResult(results);
       })
-      .catch(console.error)
+      .catch(console.error);
+  }
+
+  setRangeDate(results) {
+    this.minDate = head(results);
+    this.maxDate = last(results);
+  }
+
+  handleResult(results) {
+    this.setRangeDate(results);
   }
 }
