8081612efec3c3bebeae020aba4726740385bbc9
Paulo Luan <pauloluan.inova@gmail.com>
2019-11-21 17:20:23 -0300

    refactored layer filter reducer update function

--- src/app/tool/layer-filter-tool/layer-filter.component.ts
+++ src/app/tool/layer-filter-tool/layer-filter.component.ts
@@ -4,79 +4,44 @@ import {
   Optional,
   Inject,
   ChangeDetectorRef
-} from '@angular/core'
-import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material'
-import { FormBuilder, FormControl, Validators } from '@angular/forms'
-import { DomSanitizer } from '@angular/platform-browser'
-import { DialogComponent } from '../../dialog/dialog.component'
-import { TerrabrasilisApiComponent } from '../terrabrasilis-api/terrabrasilis-api.component'
-import { Layer } from '../../entity/layer'
-import { head, last } from 'lodash'
+} from "@angular/core";
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from "@angular/material";
+import { DomSanitizer } from "@angular/platform-browser";
+import { DialogComponent } from "../../dialog/dialog.component";
+import { TerrabrasilisApiComponent } from "../terrabrasilis-api/terrabrasilis-api.component";
+import { Layer } from "../../entity/layer";
+import { head, last } from "lodash";
 
-import { MomentDateAdapter } from '@angular/material-moment-adapter'
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE
-} from '@angular/material/core'
-import { MatDatepicker } from '@angular/material/datepicker'
-
-// Depending on whether rollup is used, moment needs to be imported differently.
-// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
-// syntax. However, rollup creates a synthetic default module and we thus need to import it using
-// the `default as` syntax.
-import * as _moment from 'moment'
-// tslint:disable-next-line:no-duplicate-imports
-import { default as _rollupMoment, Moment } from 'moment'
-
-const moment = _rollupMoment || _moment
-
-// See the Moment.js docs for the meaning of these formats:
-// https://momentjs.com/docs/#/displaying/format/
-export const MY_FORMATS = {
-  parse: {
-    dateInput: 'MM/YYYY'
-  },
-  display: {
-    dateInput: 'MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY'
-  }
-}
+import { Store, select } from "@ngrx/store";
+import * as fromLayerFilterReducer from "../../redux/reducers/layer-filter-reducer";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'layer-filter',
-  templateUrl: './layer-filter.component.html',
-  styleUrls: ['./layer-filter.component.css'],
-  providers: [
-    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
-    // application's root module. We provide it at the component level here, due to limitations of
-    // our example generation script.
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-      deps: [MAT_DATE_LOCALE]
-    },
-
-    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }
-  ]
+  selector: "layer-filter",
+  templateUrl: "./layer-filter.component.html",
+  styleUrls: ["./layer-filter.component.css"]
 })
 export class LayerFilterComponent implements OnInit {
-  dateValue = new Date(2016, 0, 1)
-  minDate = new Date(2016, 0, 1)
-  maxDate = new Date(2018, 0, 1)
+  dateValue = new Date(2016, 0, 1);
+  minDate = new Date(2016, 0, 1);
+  maxDate = new Date(2018, 0, 1);
 
-  layer: Layer
+  layer: Layer;
+
+  bindDate: Observable<Date>;
+  initialDate: Observable<Date>;
+  finalDate: Observable<Date>;
 
   constructor(
     private dialogRef: MatDialogRef<LayerFilterComponent>,
     private dom: DomSanitizer,
     private dialog: MatDialog,
     private cdRef: ChangeDetectorRef,
+    private store: Store<{ initialDate: Date }>,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.layer = data.layer
+    this.layer = data.layer;
+    this.bindDate = this.store.pipe(select("initialDate"));
   }
 
   private terrabrasilisApi: TerrabrasilisApiComponent = new TerrabrasilisApiComponent(
@@ -84,45 +49,49 @@ export class LayerFilterComponent implements OnInit {
     this.dom,
     this.cdRef,
     null
-  )
+  );
 
   ngOnInit() {}
 
   sendLayerFilter(value: any): void {
-    this.dialogRef.close()
+    this.dialogRef.close();
   }
 
   closeDialog() {
-    this.dialogRef.close()
+    this.dialogRef.close();
   }
 
   showDialog(content: string): void {
-    const dialogRef = this.dialog.open(DialogComponent, { width: '450px' })
+    const dialogRef = this.dialog.open(DialogComponent, { width: "450px" });
     dialogRef.componentInstance.content = this.dom.bypassSecurityTrustHtml(
       content
-    )
+    );
   }
 
   getDimensions() {
-    const self = this
+    const self = this;
     this.terrabrasilisApi
       .getDimensions(this.layer)
       .then(results => {
-        self.handleResult(results)
+        self.handleResult(results);
       })
-      .catch(console.error)
+      .catch(console.error);
   }
 
   setRangeDate(results) {
-    this.minDate = head(results)
-    this.maxDate = last(results)
+    this.minDate = head(results);
+    this.maxDate = last(results);
   }
 
   handleResult(results) {
-    this.setRangeDate(results)
+    this.setRangeDate(results);
   }
 
   applyFilter() {
-    console.log('apply filter')
+    const obj = {
+      initialDate: new Date(2016, 0, 1)
+    }
+    const setInitialDateAction = fromLayerFilterReducer.actions.setFilterPropsForObject(obj)
+    this.store.dispatch(setInitialDateAction);
   }
 }
