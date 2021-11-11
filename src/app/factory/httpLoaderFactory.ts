c9eba32e3dc8a7d084bd71881ca8f3a8b1beed33
André Carvalho <afacarvalho@yahoo.com.br>
2021-11-11 20:07:19 -0300

    Use the environment variable the right way

--- src/app/factory/httpLoaderFactory.ts
+++ src/app/factory/httpLoaderFactory.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../../environments/environment';
 
 export function HttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
-   let i18nResource = ( process.env.ENV === 'production' )?('/app/assets/i18n/'):('/assets/i18n/');
+   let i18nResource = ( environment.BUILD_TYPE === 'production' )?('/app/assets/i18n/'):('/assets/i18n/');
    return new TranslateHttpLoader(httpClient, i18nResource, '.json');
 }
